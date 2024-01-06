e462356345a9d9d5ec5f3c2977ae89ff987391c2
Ionela <ionela.spiridon45@gmail.com>
2024-01-06 22:32:14 +0200

    extract js in new file

---
+++ examples.js
@@ -0,0 +1,17 @@
+console.info(2 + 3);
+console.warn(" Spiridon Ionela");
+
+var employed = true;
+var age = 23;
+console.log("Angajat:", employed);
+employed = false;
+console.log("Angajat:", employed);
+employed = "da";
+console.log("Angajat:", employed);
+
+var Skills = ["html", "css"];
+console.debug("tipul variabilei skills", typeof Skills);
+
+var job = document.getElementById("job-title");
+console.warn("job", job);
+console.info(typeof job);
