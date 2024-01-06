1b7a00e95cd3880a798dc7fd1296102eae96fb77
Ionela <ionela.spiridon45@gmail.com>
2024-01-06 22:58:38 +0200

    add function examples

--- examples.js
+++ examples.js
@@ -1,17 +1,21 @@
-console.info(2 + 3);
-console.warn(" Spiridon Ionela");
+function variablesExamples() {
+  console.info(2 + 3);
+  console.warn(" Spiridon Ionela");
 
-var employed = true;
-var age = 23;
-console.log("Angajat:", employed);
-employed = false;
-console.log("Angajat:", employed);
-employed = "da";
-console.log("Angajat:", employed);
+  var employed = true;
+  var age = 23;
+  console.log("Angajat:", employed);
+  employed = false;
+  console.log("Angajat:", employed);
+  employed = "da";
+  console.log("Angajat:", employed);
 
-var Skills = ["html", "css"];
-console.debug("tipul variabilei skills", typeof Skills);
+  var Skills = ["html", "css"];
+  console.debug("tipul variabilei skills", typeof Skills);
 
-var job = document.getElementById("job-title");
-console.warn("job", job);
-console.info(typeof job);
+  var job = document.getElementById("job-title");
+  console.warn("job", job);
+  console.info(typeof job);
+}
+
+variablesExamples();
