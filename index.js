c52be5485e8b10a51d1dad381c6f74a288040662
Ionela <ionela.spiridon45@gmail.com>
2024-01-07 18:53:12 +0200

    show all pages using menu

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 function showLanguages() {
   var oldPage = document.getElementById("projects");
   oldPage.style.display = "none";
+  var oldPage = document.getElementById("HOME");
+  oldPage.style.display = "none";
+  var oldPage = document.getElementById("Skills");
+  oldPage.style.display = "none";
+
   var page = document.getElementById("languages");
+  page.style.display = "block";
+}
+
+function showHOME() {
+  var oldPage = document.getElementById("projescts");
+  oldPage.style.display = "none";
+  var oldPage = document.getElementById("languages");
+  oldPage.style.display = "none";
+  var oldPage = document.getElementById("Skills");
+  oldPage.style.display = "none";
+
+  var page = document.getElementById("HOME");
+  page.style.display = "block";
+}
+
+function showSkills() {
+  var oldPage = document.getElementById("projects");
+  oldPage.style.display = "none";
+  var oldPage = document.getElementById("HOME");
+  oldPage.style.display = "none";
+  var oldPage = document.getElementById("languages");
+  oldPage.style.display = "none";
+
+  var page = document.getElementById("Skills");
+  page.style.display = "block";
+}
+
+function showprojects() {
+  var oldPage = document.getElementById("languages");
+  oldPage.style.display = "none";
+  var oldPage = document.getElementById("HOME");
+  oldPage.style.display = "none";
+  var oldPage = document.getElementById("Skills");
+  oldPage.style.display = "none";
+
+  var page = document.getElementById("projects");
   page.style.display = "block";
 }
