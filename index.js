587bb72206c7f2d8d114eec76bf7337808097894
Ionela <ionela.spiridon45@gmail.com>
2024-01-07 22:16:37 +0200

    show all pages using menu

--- index.js
+++ index.js
@@ -1,46 +1,34 @@
 function showLanguages() {
-  var oldPage = document.getElementById("projects");
-  oldPage.style.display = "none";
-  var oldPage = document.getElementById("HOME");
-  oldPage.style.display = "none";
-  var oldPage = document.getElementById("Skills");
-  oldPage.style.display = "none";
+  document.getElementById("Skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("HOME").style.display = "none";
 
   var page = document.getElementById("languages");
   page.style.display = "block";
 }
 
 function showHOME() {
-  var oldPage = document.getElementById("projescts");
-  oldPage.style.display = "none";
-  var oldPage = document.getElementById("languages");
-  oldPage.style.display = "none";
-  var oldPage = document.getElementById("Skills");
-  oldPage.style.display = "none";
+  document.getElementById("Skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
 
   var page = document.getElementById("HOME");
   page.style.display = "block";
 }
 
 function showSkills() {
-  var oldPage = document.getElementById("projects");
-  oldPage.style.display = "none";
-  var oldPage = document.getElementById("HOME");
-  oldPage.style.display = "none";
-  var oldPage = document.getElementById("languages");
-  oldPage.style.display = "none";
+  document.getElementById("HOME").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
 
   var page = document.getElementById("Skills");
   page.style.display = "block";
 }
 
 function showprojects() {
-  var oldPage = document.getElementById("languages");
-  oldPage.style.display = "none";
-  var oldPage = document.getElementById("HOME");
-  oldPage.style.display = "none";
-  var oldPage = document.getElementById("Skills");
-  oldPage.style.display = "none";
+  document.getElementById("Skills").style.display = "none";
+  document.getElementById("HOME").style.display = "none";
+  document.getElementById("languages").style.display = "none";
 
   var page = document.getElementById("projects");
   page.style.display = "block";
