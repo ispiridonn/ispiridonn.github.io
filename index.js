0237c0f144f82b8dc0a43345634da65b11fefd65
Ionela <ionela.spiridon45@gmail.com>
2024-01-07 18:11:20 +0200

    hide old page

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function showLanguages() {
+  var oldPage = document.getElementById("projects");
+  oldPage.style.display = "none";
   var page = document.getElementById("languages");
   page.style.display = "block";
 }
